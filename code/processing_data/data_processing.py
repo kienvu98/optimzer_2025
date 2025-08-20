e3e193fdf2f148cc008949eb218a11c792f9b52b
Phạm Đăng Dương <tukyy@MacBook-Pro-cua-Pham.local>
2025-08-20 07:15:31 +0700

    embeding data

--- code/processing_data/data_processing.py
+++ code/processing_data/data_processing.py
@@ -19,6 +19,15 @@ def clean_data(text):
 def load_data(file_name):
     df = pd.read_csv(file_name)
     df['review'] = df['review'].astype(str).apply(clean_data)
-    
+    df['sentiment'] = df['sentiment'].apply(lambda x: 1 if x == 'positive' else 0)
+    return df
 
-        
+if __name__ == '__main__':
+    input_file = '/Users/tukyy/Downloads/Git/working_home/toi_uu_hoa/optimzer_2025/data/IMDB Dataset.csv'
+    output_file = '/Users/tukyy/Downloads/Git/working_home/toi_uu_hoa/optimzer_2025/data/processed_imdb.csv'
+    
+    df = load_data(input_file)
+    df.to_csv(output_file, index=False)
+    
+    print(f"Processed data saved to {output_file}")
+    print(df.head())
