91710ba255ef4400bb13ed13068fa7fab7cd7003
kienvu98 <kienkenken3@gmail.com>
2025-09-11 12:32:47 +0700

    commit

--- code/main/main.py
+++ code/main/main.py
@@ -5,7 +5,7 @@ from optimzer_project.code.model.model import Relu
 from optimzer_project.code.model.model import Sigmoid
 from optimzer_project.code.optimzer.optimzer import Momentum, GD, GD_LineSearch, Adam, SGD
 from optimzer_project.code.backend.backend import is_gpu_enable
-from optimzer_project.code.backend.utils import split_arrays, sigmoid_to_label, accuracy_score, plot_metrics
+from optimzer_project.code.backend.utils import split_arrays, sigmoid_to_label, accuracy_score, plot_metrics_optmzer
 from optimzer_project.code.backend.backend import xp as np
 from optimzer_project.code.backend.data_loader import DataLoader
 from optimzer_project.code.loss.loss import BinaryCrossEntropy
@@ -32,61 +32,64 @@ def main(file_name_X, file_name_y):
     
     # khởi tạo loss 
     loss = BinaryCrossEntropy()
-    
+
+    list_lr = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
     # danh sách các thuật toán tối ưu
     optimizers = {
-        "Adam": Adam(),
-        "GD": GD(),
-        "SGD": SGD(),
-        "Momentum": Momentum(),
-        "GD_LineSearch": GD_LineSearch(
-            lr=1.0,
-            rho=0.5,
-            c=1e-4,
-            loss_fn=lambda model: loss(model.forward(X_train), y_train),
-            model=None # sẽ gắn sau
-        )
+        #"Adam": Adam(),
+        "GD": GD()
+        #"SGD": SGD(),
+        #"Momentum": Momentum(),
+        #"GD_LineSearch": GD_LineSearch(
+        #   lr=1.0,
+        #  rho=0.5,
+        #  c=1e-4,
+        #  loss_fn=lambda model: loss(model.forward(X_train), y_train),
+        #  model=None # sẽ gắn sau
+        #)
     }
-    
     for name, optimizer in optimizers.items():
-        # tạo DataLoader
-        if name == "GD" or name == "GD_LineSearch": 
-            batch_size = 40000
-        else:
-            batch_size = 4096
+        dict_lr = {}
+        for lr in list_lr :
+            # tạo DataLoader
+            if name == "GD" or name == "GD_LineSearch": 
+                batch_size = 40000
+            else:
+                batch_size = 4096
+                
+            train_loader = DataLoader.from_arrays(X_train, y_train, batch_size=batch_size)
+            val_loader = DataLoader.from_arrays(X_test, y_test, batch_size=batch_size, shuffle=False)
+            optimizer.lr = lr    
             
-        train_loader = DataLoader.from_arrays(X_train, y_train, batch_size=batch_size)
-        val_loader = DataLoader.from_arrays(X_test, y_test, batch_size=batch_size, shuffle=False)
-        print(f"\n Đang huấn luyện với thuật toán: {name}")
+            # Khởi tạo model mới cho mỗi optimizer
+            model = Model([
+                # Dense(768, 256, optimizer, name='Dense_1'),
+                # Relu('Relu_1'),
+                Dense(768, 1, optimizer, name='Dense_1'),
+                Sigmoid('Sigmoid_1')
+            ])
+            
+            model.summary()
 
-        # Khởi tạo model mới cho mỗi optimizer
-        model = Model([
-            Dense(768, 256, optimizer, name='Dense_1'),
-            Relu('Relu_1'),
-            Dense(256, 1, optimizer, name='Dense_2'),
-            Sigmoid('Sigmoid_1')
-        ])
-        
-        model.summary()
+            # Gán model vào optimizer nếu cần (cho GD_Backtracking)
+            if hasattr(optimizer, 'model') and optimizer.model is None:
+                optimizer.model = model
 
-        # Gán model vào optimizer nếu cần (cho GD_Backtracking)
-        if hasattr(optimizer, 'model') and optimizer.model is None:
-            optimizer.model = model
+            # Khởi tạo trainer
+            trainer = Trainer(
+                model=model,
+                train_loader=train_loader,
+                val_loader=val_loader,
+                loss=loss,
+                predict_fn=sigmoid_to_label,
+                accuracy_fn=accuracy_score,
+                epochs=2000
+            )
 
-        # Khởi tạo trainer
-        trainer = Trainer(
-            model=model,
-            train_loader=train_loader,
-            val_loader=val_loader,
-            loss=loss,
-            predict_fn=sigmoid_to_label,
-            accuracy_fn=accuracy_score,
-            epochs=300
-        )
-
-        # Huấn luyện
-        trainer.fit()
-        plot_metrics(trainer, name)
+            # Huấn luyện
+            trainer.fit()
+            dict_lr[lr] = trainer.train_loss_list
+        plot_metrics_optmzer(dict_lr, name)
     
     #traine = Trainer(model=model, train_loader=train_loader,
      #                val_loader=val_loader, loss=loss,  
